4df35ec59b30acd80ca1f0abae17449e227f84c4
TGMhfock <hagen.fock@gmail.com>
2015-01-07 08:35:08 +0100

    Commands aus dem Exporter dupliziert & angepasst auf das jetzige Projekt + Testklasse + Dokumentation

---
+++ Rueckwaertssalto/src/at/salto/Commands.java
@@ -0,0 +1,150 @@
+package at.salto;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Diese Klasse lieﬂt mithilfe von der Apache CLI die angegeben Argumente aus
+ * 
+ * @author FOCK
+ *
+ */
+public class Commands {
+	private Options options;
+	private CommandLineParser parser;
+	private CommandLine cmd;
+	private HelpFormatter help;
+
+	// needed for the Exporter
+	private String host = "localhost";
+	private String user = null;
+	private String pass = null;
+	private String db = null;
+	private String table = null;
+	private String toSort = null;
+	private String sort = "ASC";
+	private String where = null;
+	private String list = null;
+	private String outputData = null;
+	private String trenn = ";";
+	private String count = null;
+	private String output = null;
+
+	/**
+	 * Der Konstruktor braucht die args von der MainKlasse
+	 * 
+	 * @param args
+	 *            Kommandoline input
+	 * 
+	 */
+	public Commands(String[] args) {
+		this.options = new Options();
+		this.help = new HelpFormatter();
+		init();
+		this.parser = new BasicParser();
+		try {
+			this.cmd = parser.parse(options, args);
+		} catch (ParseException exp) {
+			help.printHelp("The Exporter", options);
+			System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+		}
+		pars();
+
+	}
+
+	/**
+	 * In dieser Methode werden alle Optionen definiert
+	 */
+	@SuppressWarnings({ "static-access" })
+	public void init() {
+		this.options.addOption(OptionBuilder.withLongOpt("help")
+				.withDescription("-? ... Help!?\n").create("?"));
+		this.options.addOption(OptionBuilder
+				.withLongOpt("host")
+				.withDescription(
+						"-h ... Hostname des DBMS. Standard: localhost\n")
+				.withValueSeparator(' ').hasArg().create("h"));
+		this.options
+				.addOption(OptionBuilder
+						.withLongOpt("user")
+						.withDescription(
+								"-u ...Benutzername. Standard: Benutzername des im Betriebssystem angemeldeten Benutzers\n")
+						.withValueSeparator(' ').hasArg().create("u"));
+		this.options
+				.addOption(OptionBuilder
+						.withLongOpt("password")
+						.withDescription(
+								"-p ... Passwort. Alternativ kann ein Passwortprompt angezeigt werden. \n")
+						.withValueSeparator(' ').hasArg().create("p"));
+		this.options.addOption(OptionBuilder.withLongOpt("database")
+				.withDescription("-d ... Name der Datenbank")
+				.withValueSeparator(' ').hasArg().create("d"));
+	}
+
+	/**
+	 * In dieser Methode sind die if Unterscheidungen, um die angegebenen
+	 * Arguemente auszuwerten.
+	 * Dann wird ein String von NULL auf den eingesetzen Wert geaendert und
+	 * ein anderes Programm ueberprueft mit Hilfe der Getter ob die Werte z.B.
+	 * nicht NULL sind.
+	 * 
+	 */
+	public void pars() {
+		if (cmd.hasOption("?") || cmd.hasOption("help")) {
+			help.printHelp("The Exporter", options);
+		}
+		if (cmd.hasOption("h") || cmd.hasOption("host")) {
+			this.setHost(cmd.getOptionValue("h"));
+			// System.out.println(cmd.getOptionValue("h"));
+		}
+		if (cmd.hasOption("u") || cmd.hasOption("user")) {
+			this.setUser(cmd.getOptionValue("u"));
+			// System.out.println(cmd.getOptionValue("u"));
+		}
+		if (cmd.hasOption("p") || cmd.hasOption("password")) {
+			this.setPass(cmd.getOptionValue("p"));
+			// System.out.println(cmd.getOptionValue("p"));
+		}
+		if (cmd.hasOption("d") || cmd.hasOption("database")) {
+			this.setDb(cmd.getOptionValue("d"));
+			// System.out.println(cmd.getOptionValue("d"));
+		}
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getDb() {
+		return db;
+	}
+
+	public void setDb(String db) {
+		this.db = db;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+}
---
+++ Rueckwaertssalto/src/at/salto/test/TestCommands.java
@@ -0,0 +1,43 @@
+package at.salto.test;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import at.salto.Commands;
+
+import static org.junit.Assert.*;
+
+/**
+ * Das ist die Testerklasse von Commands
+ * 
+ * @author FOCK
+ *
+ */
+public class TestCommands {
+	private Commands c;
+
+	/**
+	 * 
+	 */
+	@Before
+	public void initTest() {
+		String[] args = { "-h", "localhost", "-u", "root", "-p", "1234", "-d",
+				"premiere"};
+		this.c = new Commands(args);
+		c.init();
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testParse() {
+		c.pars();
+		assertEquals("root", c.getUser());
+		assertEquals("1234", c.getPass());
+		assertEquals("localhost", c.getHost());
+		assertEquals("premiere", c.getDb());
+	}
+}
