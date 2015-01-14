8e33304251655208e8c79c80ac27eae988fc5a39
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-01-14 08:42:32 +0100

    Importiertes wieder geloescht, japanische Schriftzeichen in Code & Dokumentation machen Verwendung unmoeglich

--- Rueckwaertssalto/src/at/salto/test/astahdb/src/main/java/com/change_vision/astah/extension/plugin/dbreverse/internal/progress/ProgressMonitor.java
+++
@@ -1,9 +0,0 @@
-package com.change_vision.astah.extension.plugin.dbreverse.internal.progress;
-
-public interface ProgressMonitor {
-    
-    public void showMessage(String message);
-    
-    public void showErrorMessage(Throwable e);
-
-}
